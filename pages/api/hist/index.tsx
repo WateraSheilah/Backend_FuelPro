9c1315a287f838c6b96d6b1872be9b0e476993c5
WateraSheilah <95920248+WateraSheilah@users.noreply.github.com>
2024-06-04 02:14:14 +0300

    new
    new

--- pages/api/hist/index.tsx
+++ pages/api/hist/index.tsx
@@ -3,27 +3,22 @@ import { connectToDatabase } from '@/utils/mongodb';
 import { ObjectId } from 'mongodb';
 
 type User = {
-    _id: ObjectId;
-    username: string;
-    fuelRecordings: ObjectId[];
+    _id: ObjectId,
+    fuelRecordings: ObjectId[],
+    // Other fields...
 };
 
 type SensorReading = {
-    temperature: number;
-    sulfur: number;
-    color: string;
-    createdAt: Date;
-    stationId: ObjectId;
-};
-
-type PetrolStation = {
-    _id: ObjectId;
-    station: string;
-    location: string;
+    temperature: number,
+    sulfur: number,
+    color: string,
+    createdAt: Date,  // Date and time the reading was created
+    petrolStationName: string,  // Name of the petrol station
+    petrolStationLocation: string,  // Location of the petrol station
 };
 
 export default async function History(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'GET') {
+    if (req.method !== 'POST') {
         res.status(405).json({ error: 'Method Not Allowed' });
         return;
     }
@@ -31,7 +26,7 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
     const { username } = req.body;
 
     if (!username) {
-        res.status(400).json({ error: 'Username isn\'t logged in' });
+        res.status(400).json({ error: 'Username isnt logged in' });
         return;
     }
 
@@ -39,7 +34,6 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
         const db = await connectToDatabase();
         const usersCollection = db.collection<User>('users');
         const readingsCollection = db.collection<SensorReading>('readings');
-        const petrolStationsCollection = db.collection<PetrolStation>('petrolstation');
 
         const user = await usersCollection.findOne({ username }, { projection: { _id: 1, fuelRecordings: 1 } });
 
@@ -48,10 +42,12 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
             return;
         }
 
+        // Check if the user has no fuel recordings
         if (!user.fuelRecordings || user.fuelRecordings.length === 0) {
             res.status(200).json({ message: 'No data available for this user' });
             return;
         }
+
 
         const projection = {
             temperature: 1,
@@ -61,37 +57,14 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
             stationId: 1,
             _id: 0
         };
-
         const fuelRecordings = await readingsCollection.find(
-            { _id: { $in: user.fuelRecordings } },
+            { _id: { $in: user.fuelRecordings }},
             { projection }
         ).toArray();
 
+
         if (fuelRecordings.length > 0) {
-            const stationIds = fuelRecordings.map(reading => reading.stationId);
-            const petrolStations = await petrolStationsCollection.find(
-                { _id: { $in: stationIds } },
-                { projection: { station: 1, location: 1 } }
-            ).toArray();
-
-            const stationMap = petrolStations.reduce((map, station) => {
-                map[station._id.toString()] = {
-                    station: station.station,
-                    location: station.location,
-                };
-                return map;
-            }, {} as { [key: string]: { station: string; location: string } });
-
-            const readingsWithStationDetails = fuelRecordings.map(reading => ({
-                createdAt: reading.createdAt,
-                station: stationMap[reading.stationId.toString()]?.station || 'Unknown',
-                location: stationMap[reading.stationId.toString()]?.location || 'Unknown',
-                temperature: reading.temperature,
-                sulfur: reading.sulfur,
-                color: reading.color
-            }));
-
-            res.status(200).json({ sensorReadings: readingsWithStationDetails });
+            res.status(200).json({ sensorReadings: fuelRecordings });
         } else {
             res.status(200).json({ message: 'No sensor data available for the recorded entries' });
         }
--- pages/api/history/index.tsx
+++ pages/api/history/index.tsx
@@ -3,22 +3,27 @@ import { connectToDatabase } from '@/utils/mongodb';
 import { ObjectId } from 'mongodb';
 
 type User = {
-    _id: ObjectId,
-    fuelRecordings: ObjectId[],
-    // Other fields...
+    _id: ObjectId;
+    username: string;
+    fuelRecordings: ObjectId[];
 };
 
 type SensorReading = {
-    temperature: number,
-    sulfur: number,
-    color: string,
-    createdAt: Date,  // Date and time the reading was created
-    petrolStationName: string,  // Name of the petrol station
-    petrolStationLocation: string,  // Location of the petrol station
+    temperature: number;
+    sulfur: number;
+    color: string;
+    createdAt: Date;
+    stationId: ObjectId;
+};
+
+type PetrolStation = {
+    _id: ObjectId;
+    station: string;
+    location: string;
 };
 
 export default async function History(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
+    if (req.method !== 'GET') {
         res.status(405).json({ error: 'Method Not Allowed' });
         return;
     }
@@ -26,7 +31,7 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
     const { username } = req.body;
 
     if (!username) {
-        res.status(400).json({ error: 'Username isnt logged in' });
+        res.status(400).json({ error: 'Username isn\'t logged in' });
         return;
     }
 
@@ -34,6 +39,7 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
         const db = await connectToDatabase();
         const usersCollection = db.collection<User>('users');
         const readingsCollection = db.collection<SensorReading>('readings');
+        const petrolStationsCollection = db.collection<PetrolStation>('petrolstation');
 
         const user = await usersCollection.findOne({ username }, { projection: { _id: 1, fuelRecordings: 1 } });
 
@@ -42,12 +48,10 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
             return;
         }
 
-        // Check if the user has no fuel recordings
         if (!user.fuelRecordings || user.fuelRecordings.length === 0) {
             res.status(200).json({ message: 'No data available for this user' });
             return;
         }
-
 
         const projection = {
             temperature: 1,
@@ -57,14 +61,37 @@ export default async function History(req: NextApiRequest, res: NextApiResponse)
             stationId: 1,
             _id: 0
         };
+
         const fuelRecordings = await readingsCollection.find(
-            { _id: { $in: user.fuelRecordings }},
+            { _id: { $in: user.fuelRecordings } },
             { projection }
         ).toArray();
 
-
         if (fuelRecordings.length > 0) {
-            res.status(200).json({ sensorReadings: fuelRecordings });
+            const stationIds = fuelRecordings.map(reading => reading.stationId);
+            const petrolStations = await petrolStationsCollection.find(
+                { _id: { $in: stationIds } },
+                { projection: { station: 1, location: 1 } }
+            ).toArray();
+
+            const stationMap = petrolStations.reduce((map, station) => {
+                map[station._id.toString()] = {
+                    station: station.station,
+                    location: station.location,
+                };
+                return map;
+            }, {} as { [key: string]: { station: string; location: string } });
+
+            const readingsWithStationDetails = fuelRecordings.map(reading => ({
+                createdAt: reading.createdAt,
+                station: stationMap[reading.stationId.toString()]?.station || 'Unknown',
+                location: stationMap[reading.stationId.toString()]?.location || 'Unknown',
+                temperature: reading.temperature,
+                sulfur: reading.sulfur,
+                color: reading.color
+            }));
+
+            res.status(200).json({ sensorReadings: readingsWithStationDetails });
         } else {
             res.status(200).json({ message: 'No sensor data available for the recorded entries' });
         }
