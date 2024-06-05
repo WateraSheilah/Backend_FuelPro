844aad3255ec6b7995f40382013af2be55c43cb0
WateraSheilah <95920248+WateraSheilah@users.noreply.github.com>
2024-06-06 00:31:19 +0300

    new
    code

--- utils/mongodb.tsx
+++ utils/mongodb.tsx
@@ -4,7 +4,8 @@ import { MongoClient, Db } from 'mongodb';
 import {connect} from "node:net";
 
 //const uri = 'mongodb://localhost:27017';
-const connectionString= "mongodb+srv://hyde:An0ther12@hyde.9vshdl0.mongodb.net/";
+// const connectionString= "mongodb+srv://hyde:An0ther12@hyde.9vshdl0.mongodb.net/";
+const connectionString="mongodb://localhost:27017/";
 const dbName = 'fuelpro';
 const mClient: MongoClient = new MongoClient(connectionString);
 
